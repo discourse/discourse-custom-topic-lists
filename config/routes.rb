6c47336a7ef581174468f27bef131f5e3d379c55
Gabriel Grubba <gabriel@discourse.org>
2024-05-24 14:55:20 -0300

    UX: Update names in settings for custom topic list plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-DiscourseCustomTopicLists::Engine.routes.draw { get "lists/:topic_list_name" => "lists#show" }
+DiscourseCustomTopicLists::Engine.routes.draw do
+   get "lists/:topic_list_name" => "lists#show"
+end
 
-Discourse::Application.routes.append { mount ::DiscourseCustomTopicLists::Engine, at: "/" }
+Discourse::Application.routes.draw do
+  mount ::DiscourseCustomTopicLists::Engine, at: "custom-topic-lists"
+end
--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 # name: discourse-custom-topic-lists
-# about: TODO
+# about: add custom topic lists to your site
 # meta_topic_id: TODO
 # version: 0.0.1
 # authors: Discourse
 # url: TODO
 # required_version: 2.7.0
 
-enabled_site_setting :custom_topic_lists_enabled
+enabled_site_setting :discourse_custom_topic_lists_enabled
 
 module ::DiscourseCustomTopicLists
   PLUGIN_NAME = "discourse-custom-topic-lists"
