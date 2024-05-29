3756305d968039c5f9a243c7eb364f544d0e107f
Gabriel Grubba <gabriel@discourse.org>
2024-05-29 15:58:10 -0300

    DEV: Add test for custom topic lists

---
+++ spec/system/custom_topic_lists_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+RSpec.describe "Preset Topic Composer | preset topic creation", type: :system do
+  let!(:admin) { Fabricate(:admin, name: "Admin") }
+  fab!(:questions_category) { Fabricate(:category, name: "questions") }
+  fab!(:arts_and_media_category) { Fabricate(:category, name: "arts-media") }
+  fab!(:topic0) { Fabricate(:topic, category: questions_category) }
+  fab!(:topic1) { Fabricate(:topic, category: arts_and_media_category) }
+
+
+  before do
+    SiteSetting.discourse_custom_topic_lists_enabled = true
+    SiteSetting.experimental_topics_filter = true
+    sign_in(admin)
+  end
+
+  describe "with plugin enabled" do
+    it "should be able to access custom topic lists" do
+      visit "/"
+      expect(page).to have_selector(".list-drop")
+
+      find(".list-drop").click
+      find("li[title='New questions']").click
+
+      expect(page).to have_text("New questions")
+      expect(page).to have_text(topic0.title)
+
+      visit "/lists/arts-and-media"
+      expect(page).to have_selector(".list-drop")
+
+      expect(page).to have_text("Arts and Media")
+      expect(page).to have_text(topic1.title)
+    end
+  end
+end
