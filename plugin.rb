8f0c9d331292ab188e8cab0ba546c76332692880
Gabriel Grubba <gabriel@discourse.org>
2024-07-19 15:49:42 -0300

    DEV: move md parsing to client

--- plugin.rb
+++ plugin.rb
@@ -36,7 +36,6 @@ after_initialize do
       next false if current_user.nil?
       current_user.in_any_groups?(allowed_groups)
     end
-
-    custom_lists.each { |list| list["description"] = PrettyText.cook(list["description"]) }
+    custom_lists
   end
 end
